5880137c4dc4f5d57e8166e682985f978993da1e
Ibrahim Abdi <ibester123@gmail.com>
2019-10-13 17:25:44 -0400

    filter test

--- starter_code/filter_test.c
+++ starter_code/filter_test.c
@@ -49,8 +49,9 @@ int32_t apply2d(const filter *f, const int32_t *original, int32_t *target,
         s_row += r;
         for(int c = 0;c<f->dimension;c++){
             if((s_row >= 0) && (s_column >= 0)){
+                s_column += c;
                 sum += (f->matrix[access(r,c,f->dimension)]) * (original[access(s_row,s_column,width)]);
-                printf("sr = %d; sc = %d; r = %d; c = %d",s_row,s_column,r,c);
+                
             }
         }
     }
