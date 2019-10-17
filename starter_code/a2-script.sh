9b391c7b554178d684af55c39d1b1dabdc2b75ee
Ibrahim Abdi <ibester123@gmail.com>
2019-10-17 15:39:47 -0400

    print statement oopsie

--- starter_code/a2-script.sh
+++ starter_code/a2-script.sh
@@ -8,8 +8,8 @@ for m in 2 3 4 5
 do
     for n in 1 2 4 8 16
     do
-    echo 'hi'
-    ./main.out -t 1 -b 2 -f 3 -m ${m} -n ${n} -c ${n} > results.txt
+    foo = ./main.out -t 1 -b 2 -f 3 -m ${m} -n ${n} -c ${n}
+    ${foo} >> results.txt
     done
 done
 
