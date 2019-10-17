dd6a4e61042645244b0522202b7d818e08ca0ae1
Ibrahim Abdi <ibester123@gmail.com>
2019-10-17 15:28:41 -0400

    print statement oopsie

---
+++ starter_code/a2-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make clean ; make
+
+# pgm creator or whatevs
+
+for m in 2 3 4 5
+do
+    for n in 1 2 4 8 16
+    do
+    echo ./main.out -t 1 -b 2 -f 3 -m ${m} -n ${n} -c ${n} > results.txt
+    done
+done
+
+
