a229ec59bdb02e7c133289246af504135741946a
Ibrahim Abdi <ibester123@gmail.com>
2019-10-18 11:18:15 -0400

    scripting Pog

--- starter_code/main2.c
+++ starter_code/main2.c
@@ -83,8 +83,9 @@ int main(int argc, char **argv)
                 chunk_size = atoi(optarg);
                 break;
             case 'u':
-                print_nums = atoi(optarg):
+                print_num = atoi(optarg);
                 printf("print_num %d \n",print_num);
+                break;
             case '?':
                 print_error_arguments();
                 return 1;
@@ -185,7 +186,7 @@ int main(int argc, char **argv)
 
     if (print_time)
     {
-        printf("%d %.2lf\n",
+        printf("%d %.2lf\n",nthreads,
                 (stop.tv_sec - start.tv_sec)
                 +(double)(stop.tv_nsec - start.tv_nsec) / 1000000000
               );
