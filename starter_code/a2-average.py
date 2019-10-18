c5ac618ebf9bfaf378a08badaae61aa7c347c0cf
Ibrahim Abdi <ibester123@gmail.com>
2019-10-18 16:20:45 -0400

    ff

--- starter_code/a2-average.py
+++ starter_code/a2-average.py
@@ -59,51 +59,51 @@ write_average("result-average5.txt")
         
 #-----------------------------------------------------------------------------------------------------
 
-def edit_chunk():
-    method[1] = method[1] / 10
-    method[2] = method[2] / 10
-    method[4] = method[4] / 10
-    method[8] = method[8] / 10
-    method[16] = method[16] / 10
-    method[32] = method[32] / 10
+# def edit_chunk():
+#     method[1] = method[1] / 10
+#     method[2] = method[2] / 10
+#     method[4] = method[4] / 10
+#     method[8] = method[8] / 10
+#     method[16] = method[16] / 10
+#     method[32] = method[32] / 10
 
-def write_average_chunk(s):
-    with open(s,"a") as f:
-                    f.write("1 " + method[1] + "\n")
-                    f.write("2 " + method[2] + "\n")
-                    f.write("4 " + method[4] + "\n")
-                    f.write("8 " + method[8] + "\n")
-                    f.write("16 " + method[16] + "\n")
-                    f.write("32 " + method[32] + "\n")
+# def write_average_chunk(s):
+#     with open(s,"a") as f:
+#                     f.write("1 " + method[1] + "\n")
+#                     f.write("2 " + method[2] + "\n")
+#                     f.write("4 " + method[4] + "\n")
+#                     f.write("8 " + method[8] + "\n")
+#                     f.write("16 " + method[16] + "\n")
+#                     f.write("32 " + method[32] + "\n")
 
-with open("results2.txt","r") as fp:
-    for line in fp:
-        x = line.strip("\n")
-        if x == "thread 1":
-            method = {}
-        elif x == "thread 2":
-            edit_chunk()
-            write_average_chunk("result2-average1.txt")
-            chunk = {}
-        elif x == "thread 4":
-            edit_chunk()
-            write_average("result2-average2.txt")
+# with open("results2.txt","r") as fp1:
+#     for line in fp1:
+#         x = line.strip("\n")
+#         if x == "thread 1":
+#             method = {}
+#         elif x == "thread 2":
+#             edit_chunk()
+#             write_average_chunk("result2-average1.txt")
+#             chunk = {}
+#         elif x == "thread 4":
+#             edit_chunk()
+#             write_average("result2-average2.txt")
         
-        elif x == "thread 8":
-            edit_chunk()
-            write_average_chunk("result2-average3.txt")
-            method = {}
+#         elif x == "thread 8":
+#             edit_chunk()
+#             write_average_chunk("result2-average3.txt")
+#             method = {}
         
-        elif x == "thread 16":
-            edit_method()
-            write_average("result2-average4.txt")
-            method = {}
+#         elif x == "thread 16":
+#             edit_method()
+#             write_average("result2-average4.txt")
+#             method = {}
 
-        else:
-            y = x.split(" ")
-            thread = int(y[0])
-            time = float(y[1])
-            method[thread] += time
+#         else:
+#             y = x.split(" ")
+#             thread = int(y[0])
+#             time = float(y[1])
+#             method[thread] += time
 
-edit_chunk()
-write_average_chunk("result-average5.txt")
+# edit_chunk()
+# write_average_chunk("result-average5.txt")
