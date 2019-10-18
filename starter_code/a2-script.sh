a4467a9ff2c6281c90ea3c71ca1871fcccd30aff
Ibrahim Abdi <ibester123@gmail.com>
2019-10-18 11:25:12 -0400

    scripting Pog

--- starter_code/a2-script.sh
+++ starter_code/a2-script.sh
@@ -3,7 +3,7 @@
 make clean ; make
 
 # pgm creator or whatevers
-
+gcc -O2 -Wall -Werror main2.c pgm.c filters.c very_big_sample.o very_tall_sample.o -o main2.out -lpthread
 rm -f results.txt
 
 for m in 2 3 4 5
@@ -11,7 +11,7 @@ do
     echo method ${m} >> results.txt
     for n in 1 2 4 8 16
     do
-    ./main.out -t 1 -b 2 -f 3 -m ${m} -n ${n} -c ${n} >> results.txt
+    ./main2.out -t 1 -b 2 -f 3 -m ${m} -n ${n} -c ${n} >> results.txt
     done
 done
 
