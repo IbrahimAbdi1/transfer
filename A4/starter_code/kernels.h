8cfba5be53eb6c259b6fa9d086e6652918adaec1
Ibrahim Abdi <ibester123@gmail.com>
2019-11-22 16:40:56 -0500

    gg

--- A4/starter_code/kernels.h
+++ A4/starter_code/kernels.h
@@ -21,6 +21,7 @@
  * unfortunately, so don't use those for variable names.*/
 
 int32_t *d_min_max;
+__global__ void find_min_max(int32_t *arr,int32_t *max,int32_t *min);
 
 void run_best_cpu(const int8_t *filter, int32_t dimension, const int32_t *input,
                   int32_t *output, int32_t width, int32_t height);
