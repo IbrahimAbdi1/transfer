5afce8e4bb42611f929aa18a9a4855bb7500c5c7
Ibrahim Abdi <ibester123@gmail.com>
2019-11-29 16:46:04 -0500

    dd

--- A4/starter_code/kernel1.cu
+++ A4/starter_code/kernel1.cu
@@ -196,38 +196,38 @@ __global__ void find_min_max(int32_t *arr,int32_t *max_min,int32_t pixelCount,in
             if(max_min_data[1][threadID] > max_min_data[1][threadID+32]){max_min_data[1][threadID] = max_min_data[1][threadID+32];}
 
         }
-        
+        __syncthreads();
         if(threadID < 16){
             if(max_min_data[0][threadID] < max_min_data[0][threadID+16]){max_min_data[0][threadID] = max_min_data[0][threadID+16];}
             if(max_min_data[1][threadID] > max_min_data[1][threadID+16]){max_min_data[1][threadID] = max_min_data[1][threadID+16];}
 
         }
-        
+        __syncthreads();
         if(threadID < 8){
             if(max_min_data[0][threadID] < max_min_data[0][threadID+8]){max_min_data[0][threadID] = max_min_data[0][threadID+8];}
             if(max_min_data[1][threadID] > max_min_data[1][threadID+8]){max_min_data[1][threadID] = max_min_data[1][threadID+8];}
 
         }
-        
+        __syncthreads();
         if(threadID < 4){
             if(max_min_data[0][threadID] < max_min_data[0][threadID+4]){max_min_data[0][threadID] = max_min_data[0][threadID+4];}
             if(max_min_data[1][threadID] > max_min_data[1][threadID+4]){max_min_data[1][threadID] = max_min_data[1][threadID+4];}
 
         }
-        
+        __syncthreads();
         if(threadID < 2){
             if(max_min_data[0][threadID] < max_min_data[0][threadID+2]){max_min_data[0][threadID] = max_min_data[0][threadID+2];}
             if(max_min_data[1][threadID] > max_min_data[1][threadID+2]){max_min_data[1][threadID] = max_min_data[1][threadID+2];}
 
         }
-        
+        __syncthreads();
         if(threadID < 1){
             if(max_min_data[0][threadID] < max_min_data[0][threadID+1]){max_min_data[0][threadID] = max_min_data[0][threadID+1];}
             if(max_min_data[1][threadID] > max_min_data[1][threadID+1]){max_min_data[1][threadID] = max_min_data[1][threadID+1];}
 
         }
         
-
+        __syncthreads();
         if(tid == 0){
             printf("vlock %d max %d min %d\n", blockIdx.x,(int)max_min_data[0][0],(int)max_min_data[1][0]);
             max_min[blockIdx.x*2] = max_min_data[0][0];
