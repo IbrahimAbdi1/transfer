43e3b4746452a1a499e5e709e18d5e4bb270a662
Ibrahim Abdi <ibester123@gmail.com>
2019-11-29 20:19:18 -0500

    dd

--- A4/starter_code/kernel1.cu
+++ A4/starter_code/kernel1.cu
@@ -229,7 +229,7 @@ __global__ void find_min_max(int32_t *arr,int32_t *max_min,int32_t pixelCount,in
         
 
         if(threadID == 0){
-            printf("vlock %d max %d min %d\n", blockIdx.x,(int)max_min_data[0][0],(int)max_min_data[1][0]);
+            //printf("vlock %d max %d min %d\n", blockIdx.x,(int)max_min_data[0][0],(int)max_min_data[1][0]);
             max_min[blockIdx.x*2] = max_min_data[0][0];
             max_min[blockIdx.x*2+1] = max_min_data[1][0];
         }
