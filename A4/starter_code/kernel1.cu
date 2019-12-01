3147277425e8ef861f0349e3a53af8f8a3140b8d
Ibrahim Abdi <ibester123@gmail.com>
2019-12-01 10:28:13 -0500

    f

--- A4/starter_code/kernel1.cu
+++ A4/starter_code/kernel1.cu
@@ -32,7 +32,7 @@ void gpu_min_max_switch_threads(int pixelCount, int numThreads, int numBlocks, i
   {
     case 1024:
       if (first) {find_min_max_f<1024><<<numBlocks,numThreads,shMemSize>>>(indata, max, min,pixelCount);}
-      else find_min_max<1024><<<numBlocks,numThreads,shMemSize>>>(max, min,pixelCount);
+      else {find_min_max<1024><<<numBlocks,numThreads,shMemSize>>>(max, min,pixelCount)};
       break;
     case 512:
       if (first) {find_min_max_f<512><<<numBlocks,numThreads,shMemSize>>>(indata, max, min,pixelCount);}
