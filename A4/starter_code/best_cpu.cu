87c49afda6e303306cb27989ba4fdadf58e38fd8
Ibrahim Abdi <ibester123@gmail.com>
2019-12-01 17:37:22 -0500

    d

--- A4/starter_code/best_cpu.cu
+++ A4/starter_code/best_cpu.cu
@@ -13,6 +13,131 @@
  */
 
 #include "kernels.h"
+#include <stdio.h>
+#include <string>
+#include <unistd.h>
+#include <math.h>
+typedef struct common_work_t
+{
+    const filter *f;
+            const int32_t *original_image;
+            int32_t *output_image;
+            int32_t width;
+            int32_t height;
+            int32_t max_threads;
+            pthread_barrier_t barrier;
+            int32_t work_chunk;
+            int32_t minp;
+            int32_t maxp;
+            pthread_mutex_t lock;
+} common_work;
+typedef struct work_t
+{
+    common_work *common;
+    int32_t id;
+} work;
 
-void run_best_cpu(const int8_t *filter, int32_t dimension, const int32_t *input,
-                  int32_t *output, int32_t width, int32_t height) {}
+void run_best_cpu(const int8_t *filter, int32_t dimension, const int32_t *input,int32_t *output, int32_t width, int32_t height) {
+    common_work *x = malloc(sizeof(common_work));
+    x->f = f;
+    x->original_image = input;
+    x->output_image = output;
+    pthread_barrier_init(&(x->barrier),NULL,8);
+    pthread_mutex_init(&(x->lock), NULL);
+    x->max_threads = 8;
+    x->width = width; x->height = height;
+    x->minp = 0; x->maxp = 255;
+    pthread_t *t = (pthread_t*)malloc(8 * sizeof(pthread_t));
+
+    for(int i = 0; i < 8; i++) {
+        
+        work *y = malloc(sizeof(work));
+        y->common = x;
+        y->id = i;
+        pthread_create(&t[i], NULL,sharding_row_work , (void *)y);
+    }
+
+    for(int i = 0; i < 8; i++) {
+        pthread_join(t[i], NULL);
+    }
+
+
+}
+
+
+void *sharding_row_work(void *args){
+    work *w = (work *)args;
+    common_work *x = w->common;
+    int num_rows =w->common->height/w->common->max_threads;
+    int start_row = w->id * num_rows;
+    int end_row = start_row + num_rows;
+    double pix_min = INFINTY;
+    double pix_max = -INFINTY;
+    if(w->id == (w->common->max_threads - 1)){
+        for(int i=start_row;i<x->height;i++){
+            for(int j =0;j<w->common->width;j++){
+                int32_t new_pix = apply2d(x->f,x->original_image,x->output_image,x->width,x->height,i,j);
+                x->output_image[access(i,j,x->width)] = new_pix;
+                if(new_pix < pix_min){
+                    pix_min = new_pix;
+                }
+                else if (new_pix > pix_max){
+                    pix_max = new_pix;
+                }
+            }
+        }
+
+        pthread_mutex_lock(&(x->lock));
+        if(pix_min < x->minp){
+            x->minp = (int32_t)pix_min;
+        }
+        if (pix_max > x->maxp){
+            x->maxp = (int32_t)pix_max;
+        }
+        pthread_mutex_unlock(&(x->lock));
+
+        pthread_barrier_wait(&(x->barrier));
+        for(int i=start_row;i<x->height;i++){
+            for(int j =0;j<w->common->width;j++){
+                normalize_pixel(x->output_image,access(i,j,x->width),x->minp,x->maxp);
+            }
+        }
+
+    }
+    else{
+        for(int i=start_row;i<end_row;i++){
+            for(int j =0;j<w->common->width;j++){
+                int32_t new_pix = apply2d(x->f,x->original_image,x->output_image,x->width,x->height,i,j);
+                x->output_image[access(i,j,x->width)] = new_pix;
+                if(new_pix < pix_min){
+                    pix_min = new_pix;
+                }
+                else if (new_pix > pix_max){
+                    pix_max = new_pix;
+                }
+            }
+            
+        }
+
+        pthread_mutex_lock(&(x->lock));
+        if(pix_min < x->minp){
+            x->minp = pix_min;
+        }
+        if (pix_max > x->maxp){
+            x->maxp = pix_max;
+        }
+        pthread_mutex_unlock(&(x->lock));
+
+        pthread_barrier_wait(&(x->barrier));
+        for(int i=start_row;i<end_row;i++){
+            for(int j =0;j<w->common->width;j++){
+                normalize_pixel(x->output_image,access(i,j,x->width),x->minp,x->maxp);
+            }
+        }
+    }
+    
+    
+    
+   
+    return NULL;
+}
